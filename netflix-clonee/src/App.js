5fb0027eff8a00664bd91a4a9ec6e735a96a55fa
Musse-Wassihun <wassihunmusse10@gmail.com>
2025-04-22 22:56:52 +0300

    header and footer

--- netflix-clonee/src/App.js
+++ netflix-clonee/src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
-     <h1>hello musse wassihun</h1>
+     <Home />
     </div>
   );
 }
