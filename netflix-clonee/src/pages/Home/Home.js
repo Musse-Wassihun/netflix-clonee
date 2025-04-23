97fe3197db0ee9384c5a81af987f9a4943301aa3
Musse-Wassihun <wassihunmusse10@gmail.com>
2025-04-23 13:11:00 +0300

    row

--- netflix-clonee/src/pages/Home/Home.js
+++ netflix-clonee/src/pages/Home/Home.js
@@ -2,12 +2,15 @@ import React from 'react'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer';
 import Banner from '../../components/Banner/Banner';
+import { Rowlist } from '../../components/Rows/RowList/RowList';
+
 
 const Home = () => {
   return (
     <>
       <Header />
       <Banner />
+      <Rowlist />
       <Footer />
     </>
   );
